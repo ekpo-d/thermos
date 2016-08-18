ead159f556640b293c78cb67fcd096ae5f5fee5a
Ekpo David <ekpodave@rocketmail.com>
2016-08-18 18:10:17 +0100

    updated config file

--- thermos/config.py
+++ thermos/config.py
@@ -6,22 +6,22 @@ class Config:
     SECRET_KEY = '~t\x86\xc9\x1ew\x8bOcX\x85O\xb6\xa2\x11kL\xd1\xce\x7f\x14<y\x9e'
     DEBUG = False
 
-
+		
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'thermos.db')
+    SQLALCHEMY_DATABASE_URI = 'postgres://xfbwowihpeljve:IkNuK_U856TkT0pYeiFHuCQWIF@ec2-54-243-208-3.compute-1.amazonaws.com:5432/d8rk5mg2lfck7b'
 
 
 class TestingConfig(Config):
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+    SQLALCHEMY_DATABASE_URI = 'postgres://xfbwowihpeljve:IkNuK_U856TkT0pYeiFHuCQWIF@ec2-54-243-208-3.compute-1.amazonaws.com:5432/d8rk5mg2lfck7b'
     WTF_CSRF_ENABLED = False
     SERVER_NAME = "localhost"
 
 
 class ProductionConfig(Config):
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'thermos.db')
+    SQLALCHEMY_DATABASE_URI = 'postgres://xfbwowihpeljve:IkNuK_U856TkT0pYeiFHuCQWIF@ec2-54-243-208-3.compute-1.amazonaws.com:5432/d8rk5mg2lfck7b'
 
 
 config_by_name = dict(
